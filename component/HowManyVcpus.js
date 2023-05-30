357ab9dbb1e4ded4a265f0f0fde94c51228f6126
longsiru <mulan@wondermove.net>
2023-05-30 23:16:38 +0900

    Update HowManyVcpus.js

--- component/HowManyVcpus.js
+++ component/HowManyVcpus.js
@@ -27,16 +27,16 @@ import { useState } from "react";
 import styles from "../pages/index.module.css";
 
 const HowManyVcpus = () => {
-  const [progress, setProgress] = useState(0); // 初始进度为0
+  const [progress, setProgress] = useState(0);
   const dataPoints = [2, 4, 8, 16, 20, 32, 48, 64, 96];
 
   const handleDataPointClick = (index) => {
-    setProgress(index); // 更新进度条位置为点击的数据点的索引
+    setProgress(index);
   };
 
   const calculateTranslateX = (index) => {
-    const width = 100 / (dataPoints.length - 1); // 计算每个数据点的宽度百分比
-    return `calc(${width * index}% - ${index * 4}px)`; // 根据索引计算平移距离并返回
+    const width = 100 / (dataPoints.length - 1);
+    return `calc(${width * index}% - ${index * 4}px)`;
   };
 
   return (
@@ -45,14 +45,14 @@ const HowManyVcpus = () => {
       <div className={styles.iconCpuParent}>
         <img className={styles.iconCpu} alt="" src="/-icon-cpu.svg" />
         <div className={styles.vcpu}>{dataPoints[progress]} vCPU</div>{" "}
-        {/* 根据进度条位置显示不同的 vCPU 数量 */}
+        {/* 진행률 표시줄 위치에 따라 서로 다른 수의 vCPU 표시 */}
       </div>
       <div className={styles.rectangleParent}>
         <div
           className={styles.groupChild}
           style={{ transform: `translateX(${calculateTranslateX(progress)})` }}
         />{" "}
-        {/* 根据进度条位置设置图形的平移距离 */}
+        {/* 프로그레스 바의 위치에 따라 그래프의 평행이동 거리 설정 */}
         {dataPoints.map((dataPoint, index) => (
           <div
             key={index}
