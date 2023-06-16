93acc241b94bff70889ff6d015f2637a71b2c124
키성 <batman@wondermove.net>
2023-06-16 15:28:46 +0900

    LottieSliderBar / indicator 수정

--- component/LottieSliderBar.js
+++ component/LottieSliderBar.js
@@ -50,23 +50,25 @@
 //         <RenderTextContent currentIndex={currentPageIndex} />
 //         {/* )} */}
 //         {/* Pager */}
-// <div className={styles.ellipseParent}>
-//   {pages.map((_, index) => {
-//     const onIndicatorPress = () =>
-//       onIndicatorItemPress && onIndicatorItemPress(index);
-//     return (
-//       <div
-//         key={index}
-//         className={`${
-//           index === currentPageIndex
-//             ? styles.frameChild1
-//             : styles.ellipseDiv
-//         }`}
-//         onClick={() => onIndicatorPress(index)}
-//       />
-//     );
-//   })}
-// </div>
+{
+  /* <div className={styles.ellipseParent}>
+  {pages.map((_, index) => {
+    const onIndicatorPress = () =>
+      onIndicatorItemPress && onIndicatorItemPress(index);
+    return (
+      <div
+        key={index}
+        className={`${
+          index === currentPageIndex
+            ? styles.frameChild1
+            : styles.ellipseDiv
+        }`}
+        onClick={() => onIndicatorPress(index)}
+      />
+    );
+  })}
+</div> */
+}
 //       </div>
 //     </div>
 //   );
@@ -248,221 +250,273 @@ import lottie1 from "../asset/1.json";
 import lottie2 from "../asset/2.json";
 import lottie3 from "../asset/3.json";
 import lottie4 from "../asset/4.json";
-import { Controller, Scene } from "react-scrollmagic";
+
+const pages = [
+  {
+    title: `Identify issues and\nreduce costs with\nreal-time monitoring`,
+    content:
+      "Offer Kubernetes-based management/monitoring services with innovative technology not previously included and reduce cloud costs by leveraging eBPF and Service Mesh.",
+  },
+  {
+    title: `Intelligent Server\nForecasting for\nReduce cloud cost`,
+    content:
+      "Save costs by shifting from peak-based server calculations to a minimum specification method, supporting active parallel scale-up & scale-down, and preventing service failure during emergencies with self-validation services.",
+  },
+  {
+    title: `Resource\nOptimization\nduring DoS Attacks`,
+    content:
+      "Prevent resource waste on out-of-policy traffic, such as DDoS attacks, by utilizing eBPF's XDP technology for Linux Kernel control. (Limit the number of requests you receive from a specific IP address)",
+  },
+  {
+    title: `Optimized\nlarge-scale service\nmanagement`,
+    content:
+      "Take control with S-Kuber's intelligent services monitoring. Seamlessly oversee the flow between connected services (MSAs) and ensure reliable management of large-scale services.",
+  },
+];
+
+// Lottie array
+const lottieDataArray = [
+  {
+    animationData: lottie1,
+    className: styles.lottie1Cintainer,
+  },
+  {
+    animationData: lottie2,
+    className: styles.lottie2Cintainer,
+  },
+  {
+    animationData: lottie3,
+    className: styles.lottie3Cintainer,
+  },
+  {
+    animationData: lottie4,
+    className: styles.lottie4Cintainer,
+  },
+];
 
 const LottieSliderBar = () => {
   const [scrollPercentage, setScrollPercentage] = useState(0);
   const [currentLottieIndex, setCurrentLottieIndex] = useState(0);
   const [currentTextIndex, setCurrentTextIndex] = useState(0);
   const [currentPageIndex, setCurrentPageIndex] = useState(0);
+  const firstRef = useRef(null);
+  const secondRef = useRef(null);
+  const thirdRef = useRef(null);
+  const fourthRef = useRef(null);
 
-  const handlePagerClick = (index) => {
-    setCurrentPageIndex(index);
-    scrollToPage(index);
-  };
+  // const handlePagerClick = (index) => {
+  //   setCurrentPageIndex(index);
+  //   scrollToPage(index);
+  // };
 
-  const scrollToPage = (index) => {
-    const textContainer = document.getElementById("textContainer");
-    const pageHeight = textContainer.clientHeight;
-    textContainer.scrollTop = index * pageHeight;
-  };
+  // const scrollToPage = (index) => {
+  //   const textContainer = document.getElementById("lottieContainer");
+  //   console.log(
+  //     "🚀 ~ file: LottieSliderBar.js:309 ~ scrollToPage ~ textContainer:",
+  //     textContainer
+  //   );
+  //   const pageHeight = textContainer.clientHeight;
+  //   console.log(
+  //     "🚀 ~ file: LottieSliderBar.js:311 ~ scrollToPage ~ pageHeight:",
+  //     pageHeight
+  //   );
+  //   textContainer.scrollTop = index * pageHeight;
+  //   console.log(
+  //     "🚀 ~ file: LottieSliderBar.js:313 ~ scrollToPage ~ index * pageHeight:",
+  //     index * pageHeight
+  //   );
+  // };
 
-  useEffect(() => {
-    // const handleScroll = () => {
-    //   const textContainer = document.getElementById("textContainer");
-    //   const scrollTop = textContainer.scrollTop;
-    //   const containerHeight = textContainer.clientHeight;
-    //   const contentHeight = textContainer.scrollHeight;
-    //   const percentage = (scrollTop / (contentHeight - containerHeight)) * 100;
-    //   setScrollPercentage(percentage);
-    //   const lottieCount = 4;
-    //   const lastIndex = lottieDataArray.length - 1;
-    //   const index = Math.min(
-    //     Math.floor((percentage / 100) * lottieDataArray.length),
-    //     lastIndex
-    //   );
-    //   setCurrentLottieIndex(index);
-    //   const pageHeight = textContainer.clientHeight;
-    //   const currentPage = Math.round(textContainer.scrollTop / pageHeight);
-    //   setCurrentPageIndex(currentPage);
-    // };
-    // const textContainer = document.getElementById("textContainer");
-    // textContainer.addEventListener("scroll", handleScroll);
-    // return () => {
-    //   textContainer.removeEventListener("scroll", handleScroll);
-    // };
-  }, []);
-
-  // Lottie array
-  const lottieDataArray = [
-    {
-      animationData: lottie1,
-      className: styles.lottie1Cintainer,
-    },
-    {
-      animationData: lottie2,
-      className: styles.lottie2Cintainer,
-    },
-    {
-      animationData: lottie3,
-      className: styles.lottie3Cintainer,
-    },
-    {
-      animationData: lottie4,
-      className: styles.lottie4Cintainer,
-    },
-  ];
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const textContainer = document.getElementById("lottieContainer");
+  //     const scrollTop = textContainer.scrollTop;
+  //     const containerHeight = textContainer.clientHeight;
+  //     const contentHeight = textContainer.scrollHeight;
+  //     const percentage = (scrollTop / (contentHeight - containerHeight)) * 100;
+  //     setScrollPercentage(percentage);
+  //     const lottieCount = 4;
+  //     const lastIndex = lottieDataArray.length - 1;
+  //     const index = Math.min(
+  //       Math.floor((percentage / 100) * lottieDataArray.length),
+  //       lastIndex
+  //     );
+  //     setCurrentLottieIndex(index);
+  //     const pageHeight = textContainer.clientHeight;
+  //     const currentPage = Math.round(textContainer.scrollTop / pageHeight);
+  //     setCurrentPageIndex(currentPage);
+  //   };
+  //   const textContainer = document.getElementById("lottieContainer");
+  //   textContainer.addEventListener("scroll", handleScroll);
+  //   return () => {
+  //     textContainer.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, []);
 
   const hasContent = Boolean(lottieDataArray[currentLottieIndex]);
 
-  // const pages = [
-  //   <div className={styles.frameParent8}>
-  //     <div className={styles.identifyIssuesAndReduceCosParent}>
-  //       <div className={styles.identifyIssuesAndContainer}>
-  //         <p className={styles.wonderAbout}>Identify issues and</p>
-  //         <p className={styles.wonderAbout}>reduce costs with</p>
-  //         <p className={styles.wonderAbout}>real-time monitoring</p>
-  //       </div>
-  //       <div className={styles.offerKubernetesBasedManagemContainer}>
-  //         <p className={styles.solutionForYour}>
-  //           Offer Kubernetes-based management/monitoring services with
-  //           innovative technology not previously included and reduce cloud costs
-  //           by leveraging eBPF and Service Mesh.
-  //         </p>
-  //       </div>
-  //     </div>
-  //   </div>,
-  //   <div className={styles.frameParent10}>
-  //     <div className={styles.identifyIssuesAndReduceCosParent}>
-  //       <div className={styles.identifyIssuesAndContainer}>
-  //         <p className={styles.wonderAbout}>{`Intelligent Server `}</p>
-  //         <p className={styles.wonderAbout}>{`Forecasting for `}</p>
-  //         <p className={styles.wonderAbout}>Reduce cloud cost</p>
-  //       </div>
-  //       <div
-  //         className={styles.offerKubernetesBasedManagemContainer}
-  //       >{`Save costs by shifting from peak-based server calculations to a minimum specification method, supporting active parallel scale-up & scale-down, and preventing service failure during emergencies with self-validation services.`}</div>
-  //     </div>
-  //   </div>,
-  //   <div className={styles.frameParent8}>
-  //     <div className={styles.resourceOptimizationDuringDParent}>
-  //       <div className={styles.identifyIssuesAndContainer}>
-  //         <p className={styles.wonderAbout}>{`Resource `}</p>
-  //         <p className={styles.wonderAbout}>Optimization</p>
-  //         <p className={styles.wonderAbout}>during DoS Attacks</p>
-  //       </div>
-  //       <div className={styles.offerKubernetesBasedManagemContainer}>
-  //         <p className={styles.solutionForYour}>
-  //           {`Prevent resource waste on out-of-policy traffic, such as
-  //                     DDoS attacks, by utilizing eBPF's XDP technology for Linux
-  //                     Kernel control. (Limit the number of requests you receive
-  //                     from a specific IP address)`}
-  //         </p>
-  //       </div>
-  //     </div>
-  //   </div>,
-  //   <div className={styles.frameParent10}>
-  //     <div className={styles.resourceOptimizationDuringDParent}>
-  //       <div className={styles.identifyIssuesAndContainer}>
-  //         <p className={styles.wonderAbout}>{`Optimized `}</p>
-  //         <p className={styles.wonderAbout}>large-scale service</p>
-  //         <p className={styles.wonderAbout}>management</p>
-  //       </div>
-  //       <div className={styles.offerKubernetesBasedManagemContainer}>
-  //         <p className={styles.solutionForYour}>
-  //           {`Take control with S-Kuber's intelligent services monitoring.
-  //                   Seamlessly oversee the flow between connected services
-  //                   (MSAs) and ensure reliable management of large-scale
-  //                   services.`}
-  //         </p>
-  //       </div>
-  //     </div>
-  //   </div>,
-  // ];
-  const pages = [
-    {
-      title: `Identify issues and\nreduce costs with\nreal-time monitoring`,
-      content:
-        "Offer Kubernetes-based management/monitoring services with innovative technology not previously included and reduce cloud costs by leveraging eBPF and Service Mesh.",
-    },
-    {
-      title: `Intelligent Server\nForecasting for\nReduce cloud cost`,
-      content:
-        "Save costs by shifting from peak-based server calculations to a minimum specification method, supporting active parallel scale-up & scale-down, and preventing service failure during emergencies with self-validation services.",
-    },
-    {
-      title: `Resource\nOptimization\nduring DoS Attacks`,
-      content:
-        "Prevent resource waste on out-of-policy traffic, such as DDoS attacks, by utilizing eBPF's XDP technology for Linux Kernel control. (Limit the number of requests you receive from a specific IP address)",
-    },
-    {
-      title: `Optimized\nlarge-scale service\nmanagement`,
-      content:
-        "Take control with S-Kuber's intelligent services monitoring. Seamlessly oversee the flow between connected services (MSAs) and ensure reliable management of large-scale services.",
-    },
-  ];
-  // let currentTextIndex = 0;
+  const executeScroll = (index) => {
+    switch (index) {
+      case 0:
+        firstRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+        break;
+      case 1:
+        secondRef.current.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+        break;
+      case 2:
+        thirdRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+        break;
+      case 3:
+        fourthRef.current.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <div>
-      <Controller globalSceneOptions={{ triggerHook: "onLeave" }}>
-        {pages.map((item, index) => (
-          <div className={styles.pagerContainer} key={index}>
-            <div
-              className={`${styles.pagerContainerText} ${
-                index % 2 == 0 ? styles.frameParent8 : styles.frameParent10
-              }`}
-            >
-              <Scene
-                pin={index == pages.length - 1 ? false : true}
-                key={index}
-                duration={0}
-              >
-                <div className={styles.pagerText}>
-                  <p className={styles.wonderAbout}>{item.title}</p>
-                  <p className={styles.solutionForYour}>{item.content}</p>
-                </div>
-              </Scene>
-            </div>
-
-            <div
-              className={`${styles.lottieContainer} ${lottieDataArray[index].className}`}
-            >
-              <Lottie
-                animationData={lottieDataArray[index].animationData}
-                loop={true}
-                autoplay={true}
-                style={{ height: "1080px", width: "960px" }}
-              />
-            </div>
+      <div className={styles.pagerContainer} ref={firstRef}>
+        <div className={`${styles.pagerContainerText} ${styles.frameParent8}`}>
+          <div className={styles.pagerText}>
+            <p className={styles.wonderAbout}>{pages[0].title}</p>
+            <p className={styles.solutionForYour}>{pages[0].content}</p>
           </div>
-        ))}
-      </Controller>
-      {/* <div
+        </div>
+        <div
+          className={`${styles.lottieContainer} ${lottieDataArray[0].className}`}
+        >
+          <Lottie
+            animationData={lottieDataArray[0].animationData}
+            loop={true}
+            autoplay={true}
+            style={{ height: "1080px", width: "960px" }}
+          />
+        </div>
+        <div className={styles.ellipseParent}>
+          {pages.map((_, _index) => {
+            return (
+              <div
+                key={_index}
+                className={`${
+                  _index === 0 ? styles.frameChild1 : styles.ellipseDiv
+                }`}
+                onClick={() => executeScroll(_index)}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <div className={styles.pagerContainer} ref={secondRef}>
+        <div className={`${styles.pagerContainerText} ${styles.frameParent10}`}>
+          <div className={styles.pagerText}>
+            <p className={styles.wonderAbout}>{pages[1].title}</p>
+            <p className={styles.solutionForYour}>{pages[1].content}</p>
+          </div>
+        </div>
+
+        <div
+          className={`${styles.lottieContainer} ${lottieDataArray[1].className}`}
+        >
+          <Lottie
+            animationData={lottieDataArray[1].animationData}
+            loop={true}
+            autoplay={true}
+            style={{ height: "1080px", width: "960px" }}
+          />
+        </div>
+        <div className={styles.ellipseParent}>
+          {pages.map((_, _index) => {
+            return (
+              <div
+                key={_index}
+                className={`${
+                  _index === 1 ? styles.frameChild1 : styles.ellipseDiv
+                }`}
+                onClick={() => executeScroll(_index)}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <div className={styles.pagerContainer} ref={thirdRef}>
+        <div className={`${styles.pagerContainerText} ${styles.frameParent8}`}>
+          <div className={styles.pagerText}>
+            <p className={styles.wonderAbout}>{pages[2].title}</p>
+            <p className={styles.solutionForYour}>{pages[2].content}</p>
+          </div>
+        </div>
+
+        <div
+          className={`${styles.lottieContainer} ${lottieDataArray[2].className}`}
+        >
+          <Lottie
+            animationData={lottieDataArray[2].animationData}
+            loop={true}
+            autoplay={true}
+            style={{ height: "1080px", width: "960px" }}
+          />
+        </div>
+        <div className={styles.ellipseParent}>
+          {pages.map((_, _index) => {
+            return (
+              <div
+                key={_index}
+                className={`${
+                  _index === 2 ? styles.frameChild1 : styles.ellipseDiv
+                }`}
+                onClick={() => executeScroll(_index)}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <div className={styles.pagerContainer} ref={fourthRef}>
+        <div className={`${styles.pagerContainerText} ${styles.frameParent10}`}>
+          <div className={styles.pagerText}>
+            <p className={styles.wonderAbout}>{pages[3].title}</p>
+            <p className={styles.solutionForYour}>{pages[3].content}</p>
+          </div>
+        </div>
+
+        <div
+          className={`${styles.lottieContainer} ${lottieDataArray[3].className}`}
+        >
+          <Lottie
+            animationData={lottieDataArray[3].animationData}
+            loop={true}
+            autoplay={true}
+            style={{ height: "1080px", width: "960px" }}
+          />
+        </div>
+        <div className={styles.ellipseParent}>
+          {pages.map((_, _index) => {
+            return (
+              <div
+                key={_index}
+                className={`${
+                  _index === 3 ? styles.frameChild1 : styles.ellipseDiv
+                }`}
+                onClick={() => executeScroll(_index)}
+              />
+            );
+          })}
+        </div>
+      </div>
+
+      <div
         style={{
-          position: "absolute",
-          backgroundColor: "red",
-          zIndex: 1001,
+          position: "fixed",
           top: "50%",
-          left: "50%",
+          left: 40,
         }}
-      >
-        {pages.map((_, index) => {
-          // const onIndicatorPress = () =>
-          //   onIndicatorItemPress && onIndicatorItemPress(index);
-          return (
-            <div
-              key={index}
-              className={`${
-                index === currentPageIndex
-                  ? styles.frameChild1
-                  : styles.ellipseDiv
-              }`}
-              // onClick={() => onIndicatorPress(index)}
-            />
-          );
-        })}
-      </div> */}
+      ></div>
     </div>
   );
 };
