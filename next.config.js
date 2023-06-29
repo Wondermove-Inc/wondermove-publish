2e09d403f0fbac1949340914f6201c6367c44f87
longsiru <mulan@wondermove.net>
2023-06-29 21:50:49 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,16 +3,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/" },
-      "/P-LUG/index": { page: "/P-LUG" },
-      "/S-kuber/index": { page: "/S-kuber" },
-    };
-  },
+  //
   //   redirects: async () => {
   //     return [
   //       {
